327082398e77dde9d61d42cd4c4304dfbe90adee
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-22 23:14:18 -0400

    Exercise 3.1 - 3.2

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,31 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+
+const buttons = document.querySelector('.buttons');
+
+let animations = function (event) {
+    const btnId = event.target.id;
+    const selector = function (btnId) {
+        return document.getElementById(btnId);
+    };
+
+console.log(btnId);
+
+switch (btnId) {
+    case 'btn-1':
+        selector(btnId).style.opacity = '0';
+        break;
+    case 'btn-2':
+        selector(btnId).style.background = 'crimson';
+        break;
+    case 'btn-3':
+        selector(btnId).style.background = 'lightblue';
+        break;
+    case 'btn-4':
+        selector(btnId).classList.add('jitters');
+        break;
+    }
+};
+
+buttons.addEventListener('click', animations);
--- workshop/exercises-P3_problems/exercise-3-2/app.js
+++ workshop/exercises-P3_problems/exercise-3-2/app.js
@@ -8,3 +8,30 @@
 // Because we are now going to work with the opacity and background of the buttons,
 // they need to be set here. So that they are referenceable via the the style attribute.
 // initialize the background color for all of the buttons in here. (for loop)
+const buttons = document.querySelector('.buttons');
+
+let animations = function (event) {
+    const btnId = event.target.id;
+    const selector = function (btnId) {
+        return document.getElementById(btnId);
+    };
+
+console.log(btnId);
+
+switch (btnId) {
+    case 'btn-1':
+        selector(btnId).style.opacity = '0';
+        break;
+    case 'btn-2':
+        selector(btnId).style.background = 'crimson';
+        break;
+    case 'btn-3':
+        selector(btnId).style.background = 'lightblue';
+        break;
+    case 'btn-4':
+        selector(btnId).classList.add('jitters');
+        break;
+    }
+};
+
+buttons.addEventListener('click', animations);
