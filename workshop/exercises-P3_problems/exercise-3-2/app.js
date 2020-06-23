ddb4626f2ec1d93a01b46452db8ed1c1f756c83a
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-23 13:41:46 -0400

    Exercise 3.2 - 3.3

--- workshop/exercises-P3_problems/exercise-3-2/app.js
+++ workshop/exercises-P3_problems/exercise-3-2/app.js
@@ -10,28 +10,37 @@
 // initialize the background color for all of the buttons in here. (for loop)
 const buttons = document.querySelector('.buttons');
 
-let animations = function (event) {
-    const btnId = event.target.id;
-    const selector = function (btnId) {
-        return document.getElementById(btnId);
-    };
+//Color and opacity
 
-console.log(btnId);
+for (i = 1; i <= 4; i++) {
+    document.getElementById(`btn-${i}`).style.opacity = "100";
+    document.getElementById(`btn-${i}`).style.backgroundColor = "gold";
+  }
 
-switch (btnId) {
-    case 'btn-1':
-        selector(btnId).style.opacity = '0';
-        break;
-    case 'btn-2':
-        selector(btnId).style.background = 'crimson';
-        break;
-    case 'btn-3':
-        selector(btnId).style.background = 'lightblue';
-        break;
-    case 'btn-4':
-        selector(btnId).classList.add('jitters');
-        break;
-    }
-};
+//Removed from 3.1
+// let animations = function (event) {
+//     const btnId = event.target.id;
+//     const selector = function (btnId) {
+//         return document.getElementById(btnId);
+//     };
 
-buttons.addEventListener('click', animations);
+    const changeButtons = (event) => {
+        const btnId = event.target.id;
+        const selector = document.getElementById(btnId);
+        switch (btnId) {
+          case "btn-1":
+            selector.style.opacity = selector.style.opacity === "100" ? "0" : "100";
+            break;
+          case "btn-2":
+            selector.style.background.toggle('crimson');
+            break;
+          case "btn-3":
+            selector.style.background.toggle('light blue');
+            break;
+          case "btn-4":
+            selector.classList.toggle("jitters");
+            break;
+        }
+      };
+
+buttons.addEventListener('click', changeButtons);
